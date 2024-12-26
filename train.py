8c30c883af3f503319405756d83ed8f339ea37af
Rishi Mishra <rishimishra@Rishis-MacBook-Pro.local>
2024-12-26 11:48:13 -0700

    encoding deconding tensor

--- train.py
+++ train.py
@@ -15,8 +15,28 @@ print("size of char set: ", vocab_size)
 stoi = {ch:i for i, ch in enumerate(chars)}
 itos = {i:ch for i, ch in enumerate(chars)}
 encode = lambda s: [stoi[c] for c in s]
-# decode = lambda l: ''.join([itos[i] for i in l])
 decode = lambda l: ''.join([itos[i] for i in l])
 
 print(encode("hii there!"))
 print(decode([46, 47, 47, 1, 58, 46, 43, 56, 43, 2]))
+
+# let's now encode the entire text dataset and store it into a torch.Tensor
+import torch # we use PyTorch: https://pytorch.org
+data = torch.tensor(encode(text), dtype=torch.long)
+print(data.shape, data.dtype)
+print(data[:1000]) # the 1000 characters we looked at earier will to the GPT look like this
+
+# splitting the first 90% data into training data and rest 10% into validaiton data
+n = int(0.9 * len(data))
+train_data = data[:n]
+val_data = data[n:]
+
+block_size = 8
+train_data[:block_size+1]
+x = train_data[:block_size]
+y = train_data[1:block_size+1]
+
+for t in range(block_size):
+    context = x[:t+1]
+    target = y[t]
+    print(f"when input is {context} the target: {target}")
